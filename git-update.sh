d896ae1f663ab9801cd94576ef2525063f199a93
Mirek Grzegorczyk <mirek.grzegorczyk@gmail.com>
2016-08-20 23:14:49 +0200

    0.8

--- git-update.sh
+++ git-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
 git commit -m  "0.8"
-git remote add origin https://github.com/mirek001/smcfd.git
+git remote add origin https://gitlab.com/mirek001/smcfd.git
 git push -u origin master
